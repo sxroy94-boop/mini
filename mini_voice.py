e0211ed93e5a0ebef2d229d41eff276a2396337b
sxroy94-boop <sxroy94@gmail.com>
2026-09-21 08:33:08 +0530

    Delete mini_voice.py

--- mini_voice.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def mini_bolo(text, bhasha="bn"):
-    os.system(f'termux-tts-speak -l {bhasha} "{text}"')
-
-print("mini চালু হয়েছে...")
-mini_bolo("হ্যালো, আমি মিনি। আমি তোমার সহকারী।")
