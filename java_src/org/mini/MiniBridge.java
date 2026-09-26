66ec336566e3d92e6d5f051ea748b5ca8dc4e590
sxroy94-boop <sxroy94@gmail.com>
2026-09-26 16:28:30 +0530

    Create MiniBridge.java

---
+++ java_src/org/mini/MiniBridge.java
@@ -0,0 +1,16 @@
+package org.mini;
+
+public class MiniBridge {
+    
+    public static String hello() {
+        return "Java is alive!";
+    }
+    
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    
+    public static String echo(String text) {
+        return "Java got: " + text;
+    }
+}
